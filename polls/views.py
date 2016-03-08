cb2c10749d45455d126d6b5569e14d78c9bf125d
Jonathon Nordquist <cugamer@gmail.com>
2016-03-08 11:29:38 -0600

    Add template and rendering for poll detail view

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+
 from .models import Question
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	# This can be done by getting the template, then returning the response
+	# with the template and passing the context to the template
+	# template = loader.get_template('polls/index.html')
+	# return HttpResponse(template.render(context, request))
+
+	# This is the shortcutwhich uses the render method.
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	# return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s"
